332380ab22d85f0a693e27723479795b9a0478c7
Vedant Mahajan <vedantmahajan271@gmail.com>
2023-11-23 14:00:17 +0530

    Learning about React Hooks

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button>-</button>
+      <span>0</span>
+      <button>+</button>
+    </>
+  )
+}
+
+export default App
